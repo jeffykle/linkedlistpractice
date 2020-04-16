28983d13efc280fcc2f26f804ee52a01c5729c73
jeffykle <jeffy.kle@gmail.com>
2020-04-16 08:42:13 -0500

    Nodes selectable by click

--- static/js/main.js
+++ static/js/main.js
@@ -3,22 +3,19 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-        const svg = d3.select('#svg');
+    var current, previous = null
 
-        document.querySelector('#insert-node').onclick = ()  => {
-            load_page('insert-node')
+    const svg = d3.select('#svg');
 
-        };
+    document.querySelector('#insert-node').onclick = ()  => {
+        load_page('insert-node')
+    };
 
-        document.querySelector('#reset-list').onclick = ()  => {
-            svg.selectAll("*").remove()
-            document.querySelector('#svg').style.height = "200px"
-            load_page('reset-list')
-        };
-
-        svg.on('click', function() {
-            console.log('clicked!')
-        });
+    document.querySelector('#reset-list').onclick = ()  => {
+        svg.selectAll("*").remove()
+        document.querySelector('#svg').style.height = "200px"
+        load_page('reset-list')
+    };
 
     function draw_node(val) {
         let cx = 25+80*val
@@ -34,15 +31,22 @@ document.addEventListener('DOMContentLoaded', () => {
         cx = cx % window.innerWidth
             // console.log({"window":window.innerWidth, "x": cx, "y": cy, "box height": document.querySelector('#svg').style.height})
         // }
-
-        svg.append('circle')
+        const nodeShapes = svg.append('g')
+            .attr('class', 'node-group')
+            .attr('id', 'Node-'+val)
+            .on('click', function() {
+                            console.log(`Clicked Node-${val}!`)
+                            selectNode(val)
+                            }
+                )
+        nodeShapes.append('circle')
             .attr('id','circle-'+val)
             .attr('cx', cx)
             .attr('cy', cy)
             .attr('r', 25)
             .style('fill', 'lightblue')
 
-        svg.append("line")
+        nodeShapes.append("line")
             .attr("id", "line-"+val)
             .attr("x1", cx+25)
             .attr("y1", cy)
@@ -51,7 +55,8 @@ document.addEventListener('DOMContentLoaded', () => {
             .attr("stroke-width", 1)
             .attr("stroke", "black")
             .attr("marker-end", "url(#triangle)")
-        svg.append("svg:defs").append("svg:marker")
+        // nodeShapes.append("svg:defs").append("svg:marker")
+        nodeShapes.append("marker")
             .attr("id", "triangle")
             .attr("refX", 6)
             .attr("refY", 6)
@@ -61,7 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
             .append("path")
             .attr("d", "M 0 0 12 6 0 12 3 6")
             .style("fill", "black")
-        svg.append('text')
+        nodeShapes.append('text')
             .attr("x", cx-5*val.toString().length)
             .attr("y", cy+5)
             .attr("font-size", 20)
@@ -81,6 +86,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     };
 
+    function selectNode(val) {
+        d3.selectAll('.selected-node')
+            .classed('selected-node',false)
+        d3.selectAll('circle')
+                .style('fill', 'lightblue')
+        d3.select('#Node-'+val)
+            .selectAll('circle')
+                .style('fill', '#E8AC56')
+            .classed('selected-node',true)
+        current = {
+            "value": val,
+            "next": val+1
+        }
+        previous = {
+            "value": val == 0 ? null : val - 1,
+            "next": val+1
+        }
+        console.log(`Current: ${JSON.stringify(current)}, Previous: ${JSON.stringify(previous)}`)
+
+    }
+
     //  TODO
     function redrawNode(val) {
 
@@ -94,18 +120,43 @@ document.addEventListener('DOMContentLoaded', () => {
         request.onload = () => {
             const response = request.responseText;
             const json = JSON.parse(response)
+            current = json.current
+            previous = json.previous
 
             document.querySelector('#list-attr').innerHTML = json.attr.attr
             document.querySelector('#list-nodes').innerHTML = 'List contents: ' + json.attr.nodes
 
             if(json.current != null){
                 draw_node(json.current.value)}
-                console.log(`Current: ${JSON.stringify(json.current)}, Previous: ${JSON.stringify(json.previous)}`)
-            };
+                console.log(`Current: ${JSON.stringify(current)}, Previous: ${JSON.stringify(previous)}`)
+        };
 
 
-            request.send();
-        }
+        request.send();
+    }
+
+     /* For the drop shadow filter... */
+  var defs = svg.append("defs");
+
+  var filter = defs.append("filter")
+      .attr("id", "dropshadow")
+
+  filter.append("feGaussianBlur")
+      .attr("in", "SourceAlpha")
+      .attr("stdDeviation", 4)
+      .attr("result", "blur");
+  filter.append("feOffset")
+      .attr("in", "blur")
+      .attr("dx", 2)
+      .attr("dy", 2)
+      .attr("result", "offsetBlur");
+
+  var feMerge = filter.append("feMerge");
+
+  feMerge.append("feMergeNode")
+      .attr("in", "offsetBlur")
+  feMerge.append("feMergeNode")
+      .attr("in", "SourceGraphic");
 
 
-    });
+});
