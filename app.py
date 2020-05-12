2a966453ff7f433f6670c83b197f9fefe0a29850
jeffykle <jeffy.kle@gmail.com>
2020-05-12 11:44:53 -0500

    w/o gunicorn

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def modifyList():
     print(result)
     return result
     
-port = int(os.environ.get('PORT',33507))
 
 if __name__ == "__main__":
+    port = int(os.environ.get('PORT',33507))
     app.run(host='0.0.0.0', port=port)
